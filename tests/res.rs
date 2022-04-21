4307579b54c17ae2efa8cf34d1fda5c1049e75ee
Rico Riedel <rico.riedel@protonmail.ch>
2022-04-21 20:12:37 +0200

    Better tests

--- tests/res.rs
+++ tests/res.rs
@@ -1,33 +1,28 @@
 #[cfg(all(feature = "serde", feature = "serde_json"))]
 mod serde {
-    use std::fs;
-    use std::path::Path;
     use nonogram_rs::serde::Layout;
 
     #[test]
     fn apple() {
-        let json = fs::read_to_string(Path::new("res/apple.json")).unwrap();
-        let layout: Layout = serde_json::from_str(&json).unwrap();
-        let nonogram = layout.solve().unwrap();
+        let json = include_str!("../res/apple.json");
+        let layout: Layout = serde_json::from_str(json).unwrap();
 
-        println!("{}", nonogram);
+        layout.solve().unwrap();
     }
 
     #[test]
     fn palm_tree() {
-        let json = fs::read_to_string(Path::new("res/palm-tree.json")).unwrap();
-        let layout: Layout = serde_json::from_str(&json).unwrap();
-        let nonogram = layout.solve().unwrap();
+        let json = include_str!("../res/palm-tree.json");
+        let layout: Layout = serde_json::from_str(json).unwrap();
 
-        println!("{}", nonogram);
+        layout.solve().unwrap();
     }
 
     #[test]
     fn teal() {
-        let json = fs::read_to_string(Path::new("res/teal.json")).unwrap();
-        let layout: Layout = serde_json::from_str(&json).unwrap();
-        let nonogram = layout.solve().unwrap();
+        let json = include_str!("../res/teal.json");
+        let layout: Layout = serde_json::from_str(json).unwrap();
 
-        println!("{}", nonogram);
+        layout.solve().unwrap();
     }
 }
