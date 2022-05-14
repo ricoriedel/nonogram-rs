5d034da1a084c99f9570b4bf96a2588b0e1ea501
Rico Riedel <rico.riedel@protonmail.ch>
2022-05-14 21:37:33 +0200

    Add nonosolver binary

---
+++ src/bin/nonosolver.rs
@@ -0,0 +1,100 @@
+use std::io::{stdin, stdout, Write};
+use nonogram_rs::*;
+
+use clap::{Parser, Subcommand};
+use crossterm::QueueableCommand;
+use crossterm::style::{Color, Print, SetForegroundColor};
+
+#[derive(Parser)]
+#[clap(name = env!("CARGO_BIN_NAME"))]
+#[clap(version = env!("CARGO_PKG_VERSION"))]
+#[clap(about = include_str!("ABOUT"))]
+#[clap(author = include_str!("AUTHOR"))]
+#[clap(propagate_version = true)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Solve a nonogram and output the result
+    Solve,
+    /// Print a solved nonogram to the terminal
+    Show
+}
+
+const BAD_LAYOUT: &str = "Bad layout or json.";
+const BAD_NONOGRAM: &str = "Bad nonogram or json.";
+const INVALID_LAYOUT: &str = "Invalid layout.";
+const INVALID_COLOR: &str = "Nonogram contains invalid colors.";
+
+fn main() -> Result<(), &'static str> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Solve => solve(),
+        Command::Show => show(),
+    }
+}
+
+fn solve() -> Result<(), &'static str> {
+    let layout: Layout<char> = serde_json::from_reader(stdin()).map_err(|_| BAD_LAYOUT)?;
+    let nonogram = layout.solve().map_err(|_| INVALID_LAYOUT)?;
+    let raw: RawNonogram<char> = nonogram.into();
+
+    serde_json::to_writer(stdout(),  &raw).unwrap();
+
+    Ok(())
+}
+
+fn show() -> Result<(), &'static str> {
+    let raw: RawNonogram<char> = serde_json::from_reader(stdin()).map_err(|_| BAD_NONOGRAM)?;
+    let nonogram: Nonogram<char> = raw.try_into().map_err(|_| BAD_NONOGRAM)?;
+
+    for row in 0..nonogram.rows() {
+        for col in 0..nonogram.cols() {
+            match nonogram[(col, row)] {
+                Cell::Empty => {
+                    stdout().queue(SetForegroundColor(Color::Reset)).unwrap();
+                    stdout().queue(Print("▀▄")).unwrap();
+                }
+                Cell::Box { color } => {
+                    let c = map_color(color)?;
+
+                    stdout().queue(SetForegroundColor(c)).unwrap();
+                    stdout().queue(Print("██")).unwrap();
+                }
+                Cell::Space => {
+                    stdout().queue(Print("  ")).unwrap();
+                }
+            }
+        }
+        stdout().queue(Print("\n")).unwrap();
+    }
+    stdout().flush().unwrap();
+
+    Ok(())
+}
+
+fn map_color(color: char) -> Result<Color, &'static str> {
+    match color {
+        'd' => Ok(Color::Black),
+        'r' => Ok(Color::DarkRed),
+        'g' => Ok(Color::DarkGreen),
+        'y' => Ok(Color::DarkYellow),
+        'b' => Ok(Color::DarkBlue),
+        'm' => Ok(Color::DarkMagenta),
+        'c' => Ok(Color::DarkCyan),
+        'w' => Ok(Color::Grey),
+        'D' => Ok(Color::DarkGrey),
+        'R' => Ok(Color::Red),
+        'G' => Ok(Color::Green),
+        'Y' => Ok(Color::Yellow),
+        'B' => Ok(Color::Blue),
+        'M' => Ok(Color::Magenta),
+        'C' => Ok(Color::Cyan),
+        'W' => Ok(Color::White),
+        _ => Err(INVALID_COLOR)
+    }
+}
---
+++ tests/res.rs
@@ -0,0 +1,28 @@
+#[cfg(all(feature = "serde", feature = "serde_json"))]
+mod demo {
+    use nonogram_rs::Layout;
+
+    #[test]
+    fn apple() {
+        let json = include_str!("../res/apple.json");
+        let layout: Layout<char> = serde_json::from_str(json).unwrap();
+
+        assert!(layout.solve().is_ok());
+    }
+
+    #[test]
+    fn apple_color() {
+        let json = include_str!("../res/apple-color.json");
+        let layout: Layout<char> = serde_json::from_str(json).unwrap();
+
+        assert!(layout.solve().is_ok());
+    }
+
+    #[test]
+    fn colors() {
+        let json = include_str!("../res/colors.json");
+        let layout: Layout<char> = serde_json::from_str(json).unwrap();
+
+        assert!(layout.solve().is_ok());
+    }
+}
